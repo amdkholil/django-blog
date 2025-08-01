532adf9a1bdbe38829ce692bb2a2a8315b4b7613
amdkholil <amdkholil@gmail.com>
2025-08-01 18:15:47 +0700

    Fri Aug  1 06:15:47 PM WIB 2025

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -2,7 +2,7 @@ import sys
 import os
 
 sys.path.insert(0, os.path.expanduser('~/django-blog'))
-os.environ['DJANGO_SETTINGS_MODULE'] = 'myproject.settings'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'djBlog.settings'
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
