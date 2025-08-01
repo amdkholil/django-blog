af4474a72dda0837b7dcd7ddd03f5dea5e1ffbaa
amdkholil <amdkholil@gmail.com>
2025-08-01 18:01:01 +0700

    Fri Aug  1 06:01:01 PM WIB 2025

--- djBlog/settings.py
+++ djBlog/settings.py
@@ -149,9 +149,10 @@ MDEDITOR_CONFIGS = {
                     "bold", "del", "italic", "quote", "ucwords", "uppercase", "lowercase", "|",
                     "h1", "h2", "h3", "h5", "h6", "|",
                     "list-ul", "list-ol", "hr", "|",
-                    "link", "reference-link", "image", "code", "preformatted-text", "code-block", "table", "datetime",
+                    "link", "reference-link", "image", "code", # "preformatted-text", 
+                    "code-block", "table", "datetime",
                     "emoji", "html-entities", "pagebreak", "goto-line", "|",
-                    "help", "info",
+                    # "help", "info",
                     "||", "preview", "watch", "fullscreen"],  # custom edit box toolbar 
         'upload_image_formats': ["jpg", "jpeg", "gif", "png", "bmp", "webp"],
         'image_folder': 'editor',  # image save the folder name
@@ -161,9 +162,9 @@ MDEDITOR_CONFIGS = {
         'toolbar_autofixed': True,  # Whether the toolbar capitals
         'search_replace': True,  # Whether to open the search for replacement
         'emoji': True,  # whether to open the expression function
-        'tex': True,  # whether to open the tex chart function
-        'flow_chart': True,  # whether to open the flow chart function
-        'sequence': True, # Whether to open the sequence diagram function
+        'tex': False,  # whether to open the tex chart function
+        'flow_chart': False,  # whether to open the flow chart function
+        'sequence': False, # Whether to open the sequence diagram function
         'watch': True,  # Live preview
         'lineWrapping': False,  # lineWrapping
         'lineNumbers': False,  # lineNumbers
